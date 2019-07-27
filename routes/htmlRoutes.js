82e0f52f5b8d6a348393c39a835ea6284ae74025
Andrew Joung <andrew.joung@gmail.com>
2019-07-27 12:58:13 -0700

    add gitignore

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,10 @@
+var db = require("../models");
+var axios = require('axios');
+
+module.exports = function(app) {
+  // Load index page
+  app.get("/", function(req, res) {
+    res.render("../public/index.html");
+  });
+    
+};
